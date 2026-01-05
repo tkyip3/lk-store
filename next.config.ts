c2b1f7223da417cacb5ab1a8f17199c46d20b53c
tkyip3 <tkyip3@gmail.com>
2026-01-05 23:03:52 +0800

    next config update

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,30 @@
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
 import { withPayload } from '@payloadcms/next/withPayload'
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
 
 if (process.env.NODE_ENV === 'development') {
   initOpenNextCloudflareForDev({})
 }
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
   webpack: (webpackConfig: any) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
     return webpackConfig
   },
-  turbopack: { resolveExtensions: ['.cts', '.cjs', '.ts', '.tsx', '.js', '.jsx', '.mts', '.mjs'] },
+  turbopack: {
+    resolveExtensions: ['.cts', '.cjs', '.ts', '.tsx', '.js', '.jsx', '.mts', '.mjs'],
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_PAYLOAD_API, // 或你图床的域名
-        // port: '', // 可选
-        // pathname: '/**', // 可选
+        hostname: 'lk-store-production.tkyip3.workers.dev',
       },
-      // 若有其他域名也要加进来
-    ],
+    ], // 移除 `as const`
   },
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig as any, { devBundleServerPackages: false })
