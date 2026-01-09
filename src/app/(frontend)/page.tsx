7f2e96c98593b62ec76459e1d71c9ee24fedd6d7
tkyip3 <tkyip3@gmail.com>
2026-01-10 00:51:38 +0800

    lot of layout update

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,58 +1,63 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
+import Link from 'next/link'
+import Image from 'next/image'
 
-import config from '@/payload.config'
+import type { Product } from '@/payload-types'
+
+import GlassFilter from '@/components/GlassFilter'
+
 import './styles.css'
 
+async function getProducts(): Promise<{ docs: Product[] }> {
+  const url = `${process.env.NEXT_PUBLIC_PAYLOAD_API}/api/products?where[published][equals]=true&locale=zh-TW&limit=10&sort=-createdAt`
+  const res = await fetch(url, { next: { revalidate: 30 } })
+  if (!res.ok) throw new Error('Failed to fetch products')
+  return res.json()
+}
+
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
+  const { docs: products } = await getProducts()
   return (
     <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
+      <div className="container mx-auto">
+        <div className="home-products">
+          <div className="divider text-4xl my-8 font-bold">最新商品</div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+            {products.map((p: Product) => (
+              <div key={p.id} className="card bg-base-100/50 backdrop-blur-sm w-full shadow-sm">
+                <figure className="hover-gallery aspect-square relative">
+                  {p.images?.map((img) => {
+                    if (typeof img.image === 'object' && img.image?.url) {
+                      return (
+                        <Image
+                          key={img.id}
+                          src={img.image.url}
+                          alt={p.name}
+                          fill
+                          className="object-cover"
+                          unoptimized
+                        />
+                      )
+                    }
+                  })}
+                </figure>
+
+                <div className="card-body">
+                  <h2 className="card-title">{p.name}</h2>
+
+                  <p>
+                    {p.price} {p.currency.toUpperCase()}
+                  </p>
+                  <div className="card-actions justify-end">
+                    <Link href={`/products/${p.slug}`} className="btn btn-primary">
+                      查看詳情
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
       </div>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,26 @@
+import GlassFilter from '@/components/GlassFilter'
 export default function Header() {
   return (
-    <header className="navbar bg-base-100 shadow-md sticky top-0 z-50">
-      <div className="navbar-start">
-        <a href="/" className="btn btn-ghost normal-case text-xl">
-          <img src="/images/header/logo.png" alt="Logo" className="h-8 w-auto" />
-        </a>
+    <header className="">
+      <div className="container mx-auto">
+        <div className="header-wrapper">
+          <GlassFilter>
+            <div className="wrapper-main">
+              <a href="/" className="logo">
+                <img src="/images/header/logo.png" alt="Logo" />
+              </a>
+              <div className="header-menu">
+                <a href="/products" className="menu-item">
+                  所有貨品
+                </a>
+                <a href="/categories" className="menu-item">
+                  所有分類
+                </a>
+              </div>
+            </div>
+          </GlassFilter>
+        </div>
       </div>
-      {/* 可從 Payload API 抓選單項目（進階） */}
     </header>
   )
 }
